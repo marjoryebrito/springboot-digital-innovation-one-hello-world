9ebb97afc42b9efdc68e7038837ed38abc283a43
marjoryeads <marjorye.ads@gmail.com>
2021-09-17 18:24:12 -0300

    Aula 1 - O que é Spring Boot

---
+++ src/main/java/com/digitalinovetionone/fatjar/FatjarApplication.java
@@ -0,0 +1,13 @@
+package com.digitalinovetionone.fatjar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FatjarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FatjarApplication.class, args);
+	}
+
+}
